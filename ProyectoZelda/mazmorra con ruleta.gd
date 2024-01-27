4753977e1073ba80b8adf9762af652e1a6777cf2
Hectovargas <fefovargitas@gmail.com>
2024-01-27 17:45:37 -0600

    cambios dabado

--- ProyectoZelda/mazmorra con ruleta.gd
+++ ProyectoZelda/mazmorra con ruleta.gd
@@ -1,9 +1,28 @@
 extends Node2D
 var animated_sprite = AnimatedSprite2D.new()
 var ruleteado = false;
+var dialog = false
+var texto = "Aparentemente he caido en una trampa"
+var i = 0
+var j = 0
+var textos=""
+var empezar = false
 func _ready():
 	pass
 func _process(delta):
+	if $TileMap/Jugador.position.x >= 450 && $TileMap/Jugador.position.x <= 550 && $TileMap/Jugador.position.y <= 650 && $TileMap/Jugador.position.y >= 550 && dialog == false:
+		if i<36:
+			$TileMap/Jugador/NinePatchRect.show()
+			textos = $TileMap/Jugador/NinePatchRect/Label.text+(texto[i])
+			$TileMap/Jugador/NinePatchRect/Label.set_text(textos)
+			
+		else:
+			var dialog = true
+			if Input.is_key_pressed(KEY_ENTER):
+				$TileMap/Jugador/NinePatchRect.hide()
+		i+=1
+	else:
+		$TileMap/Jugador/NinePatchRect.hide()
 	var numeroaleatorio
 	if $TileMap/Jugador.position.x >= 450 && $TileMap/Jugador.position.x <= 550 && $TileMap/Jugador.position.y <= 450 && $TileMap/Jugador.position.y >= 350:
 		if(ruleteado == false):
@@ -20,13 +39,43 @@ func _process(delta):
 		var current_frame = $TileMap/AnimatedSprite2D.get_frame()
 		var random = RandomNumberGenerator.new()
 		numeroaleatorio = random.randi_range(0, 400000)
-		print(current_frame)
-		print(numeroaleatorio)
-		if(numeroaleatorio<=100 && numeroaleatorio<=1000  && current_frame == 0):
+		
+		if(numeroaleatorio>=0 && numeroaleatorio<=4000  && current_frame == 0):
 			$TileMap/AnimatedSprite2D.pause()
-		if(numeroaleatorio<=1001 && numeroaleatorio<=2000 && current_frame == 2):
+			$TileMap/Jugador/Camera2D/TextureRect/Label.set_text("El resultado es: CIENCIA")
+			$TileMap/Jugador/Camera2D/TextureRect.show()
+			$Sprite2D.show()
+			$StaticBody2D2.position = Vector2(0,0)
+			empezar=true
+			
+		if(numeroaleatorio>=4001 && numeroaleatorio<=8000 && current_frame == 2):
 			$TileMap/AnimatedSprite2D.pause()
-		if(numeroaleatorio<=2001 && numeroaleatorio<=3000 && current_frame == 4):
+			$TileMap/Jugador/Camera2D/TextureRect/Label.set_text("El resultado es: ARTE")
+			$TileMap/Jugador/Camera2D/TextureRect.show()
+			$Sprite2D2.show()
+			$StaticBody2D3.position = Vector2(0,0)
+			empezar=true
+
+		if(numeroaleatorio>=80001 && numeroaleatorio<=12000 && current_frame == 4):
 			$TileMap/AnimatedSprite2D.pause()
-		if(numeroaleatorio<=3001 && numeroaleatorio<=4000 && current_frame == 6):
+			$TileMap/Jugador/Camera2D/TextureRect/Label.set_text("El resultado es: HISTORIA")
+			$TileMap/Jugador/Camera2D/TextureRect.show()
+			$Sprite2D3.show()
+			$StaticBody2D4.position = Vector2(0,0)
+			empezar=true
+			
+		if(numeroaleatorio>=12001 && numeroaleatorio<=16000 && current_frame == 6):
 			$TileMap/AnimatedSprite2D.pause()
+			
+			$TileMap/Jugador/Camera2D/TextureRect/Label.set_text("El resultado es: POLITICA")
+			$TileMap/Jugador/Camera2D/TextureRect.show()
+			$Sprite2D4.show()
+			$StaticBody2D5.position = Vector2(0,0)
+			empezar=true
+	if j<251:
+		if empezar == true:
+			j+=1
+			print (j)
+			if j > 250:
+				$TileMap/Jugador/Camera2D/TextureRect.hide()
+				empezar == false
